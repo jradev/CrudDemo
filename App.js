afbd683ec63d973c9b639455102c2dc388d8ed61
Jomari Iringan <jom.iringan@gmail.com>
2022-12-01 18:12:58 +0800

    initial app repository setup

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <StatusBar
+        barStyle={'dark-content'}
+      />
+      
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
